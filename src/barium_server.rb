f9fa65b8bf3fb5a41862a5a80c49f76f06bf07b0
Justin Bozonier <darkxanthos@gmail.com>
2012-04-13 15:50:50 -0700

    adding error handling

--- src/barium_server.rb
+++ src/barium_server.rb
@@ -5,34 +5,38 @@ require "uuid"
 set :public_folder, "../logs"
 
 get "/" do
-  puts "Starting to track"
-  if cookies[:barium_trace] == nil
-      puts "No cookie! Setting..."
-      response.set_cookie("barium_trace",
-                 :value => UUID.generate(),
-                 :domain => '127.0.0.1',
-                 :path => "/",
-                 :expires => Time.new(2020,1,1))
-      puts "Set the cookie!"
-  else
-    puts "Found cookie! Here: #{cookies[:barium_trace]}"
+  begin
+    puts "Starting to track"
+    if cookies[:barium_trace] == nil
+        puts "No cookie! Setting..."
+        response.set_cookie("barium_trace",
+                   :value => UUID.generate(),
+                   :domain => '127.0.0.1',
+                   :path => "/",
+                   :expires => Time.new(2020,1,1))
+        puts "Set the cookie!"
+    else
+      puts "Found cookie! Here: #{cookies[:barium_trace]}"
+    end
+
+    current_time = Time.now
+    log_folder_path = "../logs"
+    log_file_path = "#{log_folder_path}/log_#{current_time.hour}.txt"
+
+    if not File.directory?(log_folder_path)
+      Dir.mkdir log_folder_path
+    end
+
+    trace_id = cookies[:barium_trace]
+
+    (file = File.new(log_file_path,'a')).flock(File::LOCK_EX)
+    file.puts("#{current_time}\t#{trace_id}")
+    file.flock(File::LOCK_UN)
+    file.close
+
+    puts "Ending tracking"
+  rescue Exception => error
+    return error
   end
-
-  current_time = Time.now
-  log_folder_path = "../logs"
-  log_file_path = "#{log_folder_path}/log_#{current_time.hour}.txt"
-
-  if not File.directory?(log_folder_path)
-    Dir.mkdir log_folder_path
-  end
-
-  trace_id = cookies[:barium_trace]
-
-  (file = File.new(log_file_path,'a')).flock(File::LOCK_EX)
-  file.puts("#{current_time}\t#{trace_id}")
-  file.flock(File::LOCK_UN)
-  file.close
-
-  puts "Ending tracking"
   "hai"
 end
