433e64329de35306723d20f5a2f8f33d36e5c2a8
Justin Bozonier <darkxanthos@gmail.com>
2012-05-09 19:28:22 -0400

    Added ability to pull logs from load balanced servers.

--- utilities/log_archiver.rb
+++ utilities/log_archiver.rb
@@ -3,44 +3,50 @@ require 'JSON'
 
 def archive_files
 	#server_root = "127.0.0.1:9292"
-	server_root = "logbalancer-231309745.us-east-1.elb.amazonaws.com"
+	
 	log_directory = "c:/Barium"
+	server_index = 0
+	servers = ['ec2-50-19-179-243.compute-1.amazonaws.com', 'ec2-107-21-85-95.compute-1.amazonaws.com']
+	servers.each do |server_root|
+		puts "archiving from server #{server_root}"
+		json_files_to_archive = HTTParty.get("http://#{server_root}/files_to_archive")
+		files_to_archive = JSON.parse(json_files_to_archive)
 
-	json_files_to_archive = HTTParty.get("http://#{server_root}/files_to_archive")
-	files_to_archive = JSON.parse(json_files_to_archive)
+		all_files_downloaded_correctly = true
 
-	all_files_downloaded_correctly = true
+		Dir.mkdir log_directory unless File.directory? log_directory
 
-	Dir.mkdir log_directory unless File.directory? log_directory
+		files_to_archive.each do |file_to_archive|
+			file_name = server_index.to_s + "_" + File.basename(file_to_archive["uri"])
+			puts "Starting log file download for #{file_name}"
+			log_file_path = File.join(log_directory, file_name)
 
-	files_to_archive.each do |file_to_archive|
-		file_name = File.basename file_to_archive["uri"]
-		puts "Starting log file download for #{file_name}"
-		log_file_path = File.join(log_directory, file_name)
+			if File.exists?(log_file_path) and files_match File.size(log_file_path), file_to_archive["size"]
+				puts "skipping download #BecauseRacecar"
+				next
+			end
+			
+			log_file_text = HTTParty.get(file_to_archive["uri"])
 
-		if File.exists?(log_file_path) and files_match File.size(log_file_path), file_to_archive["size"]
-			puts "skipping download #BecauseRacecar"
-			next
+			File.open(log_file_path, "w") do |file|
+				file.write log_file_text
+			end
+			if files_match File.size(log_file_path), file_to_archive["size"]
+				puts "log download success!" 
+			else
+				puts "log download FAIL! local #{File.size(log_file_path)} vs server #{file_to_archive["size"]}" 
+				all_files_downloaded_correctly = false
+			end
 		end
 		
-		log_file_text = HTTParty.get(file_to_archive["uri"])
-
-		File.open(log_file_path, "w") do |file|
-			file.write log_file_text
+		if all_files_downloaded_correctly
+			puts "Cleaning out the archived log files!"
+			HTTParty.get("http://#{server_root}/clean") 
+		else 
+			puts "Shit went sour! Not clearing the logs."
 		end
-		if files_match File.size(log_file_path), file_to_archive["size"]
-			puts "log download success!" 
-		else
-			puts "log download FAIL! local #{File.size(log_file_path)} vs server #{file_to_archive["size"]}" 
-			all_files_downloaded_correctly = false
-		end
-	end
-	
-	if all_files_downloaded_correctly
-		puts "Cleaning out the archived log files!"
-		HTTParty.get("http://#{server_root}/clean") 
-	else 
-		puts "Shit went sour! Not clearing the logs."
+		
+		server_index += 1
 	end
 end
 
